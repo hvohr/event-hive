2c4d844b27113eebe0d61f3519885db1fb3f53f3
Hollis Vohr <hollis.vohr@gmail.com>
2023-12-19 11:31:27 -0700

    feat: add auth and root folders

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,6 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <main>
-      <Button variant='default' className="px-10">Delete</Button>
     </main>
   )
 }
